56fb1a8402f9c6fe9caa3fb36aa197c84e21dc1e
Aquarius-dev-max <otienomichaelsons789@gmail.com>
2024-11-23 10:53:43 +0300

    Add files via upload

---
+++ products.js
@@ -0,0 +1,599 @@
+const products = [
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image: "images copy/1.png",
+    name: "Laptop",
+    rating: {
+      stars: 4.5,
+      count: 87
+    },
+    priceCents: 30000,
+    keywords: [
+      "laptop",
+      "laptops",
+      "HP"
+    ]
+  },
+  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/2.png",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },
+  {
+    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    image: "3.png",
+    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    rating: {
+      stars: 4.5,
+      count: 56
+    },
+    priceCents: 799,
+    keywords: [
+      "tshirts",
+      "apparel",
+      "mens"
+    ],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png"
+  },
+  {
+    id: "54e0eccd-8f36-462b-b68a-8182611d9add",
+    image: "4.png",
+    name: "2 Slot Toaster - Black",
+    rating: {
+      stars: 5,
+      count: 2197
+    },
+    priceCents: 1899,
+    keywords: [
+      "toaster",
+      "kitchen",
+      "appliances"
+    ]
+  },
+  {
+    id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
+    image: "5.png",
+    name: "6 Piece White Dinner Plate Set",
+    rating: {
+      stars: 4,
+      count: 37
+    },
+    priceCents: 2067,
+    keywords: [
+      "plates",
+      "kitchen",
+      "dining"
+    ]
+  },
+  {
+    id: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
+    image: "6.png",
+    name: "6-Piece Nonstick, Carbon Steel Oven Bakeware Baking Set",
+    rating: {
+      stars: 4.5,
+      count: 175
+    },
+    priceCents: 3499,
+    keywords: [
+      "kitchen",
+      "cookware"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/3.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/4.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/5.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/6.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/7.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/8.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/9.jpgg",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/10.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/11.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/12.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/ailyonsg",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/amazon tablet.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/amtec tv.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/apple.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/asus.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/BT Speaker.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/2.png",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/2.png",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/2.png",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/2.png",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/2.png",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/2.png",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/2.png",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/2.png",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/2.png",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/2.png",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/2.png",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/2.png",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/2.png",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/2.png",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/2.png",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/2.png",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/2.png",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/2.png",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/2.png",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/2.png",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/2.png",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/2.png",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images copy/2.png",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },
+];
